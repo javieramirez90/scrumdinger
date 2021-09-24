406fea3eed28ed200e51c9f0b596d4cbfafca08d
javier <javier@dojodevs.io>
2021-09-24 00:19:53 -0500

    first commit

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MeetingView: View {
     var body: some View {
-        Text("Hello, world!")
+        ProgressView(value: 5, total: 25)
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MeetingView()
     }
 }
